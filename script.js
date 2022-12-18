a16adf7f9aaabf4defa254bbafefa91bc0a961c2
Emmanuel Gonzalez <L_EmmanuelGonzalez@outlook.com>
2022-12-18 01:37:49 -0300

    correcciones

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 let prestamo = [
-  { nombre: "credito", precio: 0, categoria: "Nulo" },
-  { nombre: "credito", precio: 5000, categoria: "Chico" },
-  { nombre: "credito", precio: 10000, categoria: "mediano" },
-  { nombre: "credito", precio: 100000, categoria: "grande" }
+    { nombre: "credito", precio: 0, categoria: "Nulo" },
+    { nombre: "credito", precio: 5000, categoria: "Chico" },
+    { nombre: "credito", precio: 10000, categoria: "mediano" },
+    { nombre: "credito", precio: 100000, categoria: "grande" }
 ]
 
 let total = 0
 
 for (let i = 0; i < prestamo.length; i++) {
-  console.log(prestamo[i].precio)
-  total = total + prestamo[i].precio
+    console.log(prestamo[i].precio)
+    total = total + prestamo[i].precio
 }
 console.log("El total es: ", total)
 console.log("------------")
 total = 0
 
 for (const prestamo of prestamo) {
-  console.log(prestamo.precio)
-  total = total + prestamo.precio
+    console.log(prestamo.precio)
+    total = total + prestamo.precio
 }
 
 let name = prompt("Ingrese su nombre")
@@ -33,19 +33,19 @@ if (edad >= 18) {
     alert("Monto a solicitar: " + MontoASolicitar + "\n" + "Interes por financiacion: " + "\n" + "Monto total financiado: " + MontoAPagar.toFixed(2) + "\n" + "En Cantidad de cuotas: " + CantidadDeCuotas + "\n" + "Monto de cada cuota: " + (MontoAPagar / CantidadDeCuotas).toFixed(2))
 
     let opcion = Number(prompt("Ingrese 1 para aceptar el préstamo \n ingrese 2 para calcular de nuevo \n ingrese 0 para salir"))
-    if (opcion == 0) { alert("GRACIAS POR VISITARNOS, VUELVE PRONTO")}
+    if (opcion == 0) { alert("GRACIAS POR VISITARNOS, VUELVE PRONTO") }
     while (opcion != 0) {
         if (opcion == 1) {
-             alert("Su préstamo fue aprobado con éxito, disfrute de su dinero")
-            } else if (opcion == 2) {
+            alert("Su préstamo fue aprobado con éxito, disfrute de su dinero")
+        } else if (opcion == 2) {
             let MontoASolicitar = Number(prompt("ingrese monto a solicitar"))
             let MontoAPagar = MontoASolicitar * 1.75
             let CantidadDeCuotas = Number(prompt("Ingrese el numero de cuotas que desea pagar su préstamo personal"))
-    
+
             alert("Monto a solicitar: " + MontoASolicitar + "\n" + "Interes por financiacion: " + "\n" + "Monto total financiado: " + MontoAPagar.toFixed(2) + "\n" + "En Cantidad de cuotas: " + CantidadDeCuotas + "\n" + "Monto de cada cuota: " + (MontoAPagar / CantidadDeCuotas).toFixed(2))
-    
-    
+
+
         }
-    
+
     }
-}else {alert("Eres menor de edad, no puedes continuar")}
+} else { alert("Eres menor de edad, no puedes continuar") }
