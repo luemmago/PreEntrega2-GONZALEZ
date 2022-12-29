e74b356bbe3ebf5869a9718d1990c38e0287bd34
Emmanuel Gonzalez <L_EmmanuelGonzalez@outlook.com>
2022-12-29 03:44:30 -0300

    renderizar

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ let productos = [
   { id: 9, categorias: ["Indumentaria"], nombre: "Bordado de la 3era estrella mas la insignia del campeon", stock: 10000, precio: 6000, imgUrl: "https://images.pagina12.com.ar/styles/focal_3_2_470x313/public/2022-12/681721-48233-camiseta-2520-25282-2529.jpeg?itok=B1qtsBsO" },
 ]
 
-let contenedor = document.getElementById("contenedorProductos")
 let carrito = []
 let contenedorCarrito = document.getElementById("contenedorCarrito")
 
+let contenedor = document.getElementById("contenedorProductos")
 renderizarProductos(productos)
 
 let buscador = document.getElementById("buscador")
@@ -26,7 +26,7 @@ function renderizarProductos(arrayDeProductos) {
     let tarjetaProducto = document.createElement("div")
     tarjetaProducto.className = "producto"
     tarjetaProducto.id = producto.id
-
+  
     tarjetaProducto.innerHTML = `
       <h3>${producto.nombre}</h3>
       <p>Quedan ${producto.stock} u.</p>
@@ -36,4 +36,40 @@ function renderizarProductos(arrayDeProductos) {
     `
     contenedor.appendChild(tarjetaProducto)
   }
+  let botones = document.getElementsByClassName("boton")
+  for (const boton of botones) {
+    boton.addEventListener("click", agregarAlCarrito)
+  }
 }
+
+function agregarAlCarrito(e) {
+  let productoBuscado = productos.find(producto => producto.id == e.target.id)
+  let posicionDelProductoBuscado = carrito.findIndex(producto => producto.id == productoBuscado.id)
+  if (posicionDelProductoBuscado != -1) {
+    carrito[posicionDelProductoBuscado].unidades++
+    carrito[posicionDelProductoBuscado].subtotal = carrito[posicionDelProductoBuscado].unidades * carrito[posicionDelProductoBuscado].precioUnitario
+  } else {
+    carrito.push({id: productoBuscado.id, nombre: productoBuscado.nombre, precioUnitario: productoBuscado.precio, unidades: 1, subtotal: productoBuscado.precio})
+  }
+  renderizarCarrito(carrito)
+}
+
+function renderizarCarrito(arrayDeProductos) {
+  contenedorCarrito.innerHTML = ''
+  for (const producto of arrayDeProductos) {
+    contenedorCarrito.innerHTML += `
+      <div class="flex">
+        <p>${producto.nombre}</p>
+        <p>${producto.precioUnitario}</p>
+        <p>${producto.unidades}</p>
+        <p>${producto.subtotal}</p>
+      </div>
+    `
+  }
+
+  let total = carrito.reduce((acc, valorActual) => acc + valorActual.subtotal, 0)
+  contenedorCarrito.innerHTML += `
+    <h3>TOTAL $${total}</h3>
+  `
+}
+
